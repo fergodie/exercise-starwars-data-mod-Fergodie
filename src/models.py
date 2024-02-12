4c5ea00c8ddc511fe3114f8429728e6f792ed3ee
Fergodie <Fero.die@gmail.com>
2024-02-12 20:38:39 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -1,30 +1,53 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    mail = Column(String(100), unique=True, nullable=False)
+    nombre = Column(String(100), nullable=False)
+    contraseña = Column(String(100), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, nullable=False)
+    nombre = Column(String(100), nullable=False)
+    color = Column(Enum)
+    tamaño = Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {}
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    nombre = Column(String(100), nullable=False)
+    color = Column(String(100), nullable=False)    
+    
+    def to_dict(self):
+        return {}
+    
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    usuarioId = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    planetasId = Column(String(50), ForeignKey('planetas.id'), nullable=True)
+    personajesId = Column(String(50), ForeignKey('personajes.id'), nullable=True)
+    
     def to_dict(self):
         return {}
 
